fcf5d856687ad7bb49af85729e096d9ed227dd0e
deepak <deepak@excellencetechnologies.in>
2020-08-12 13:23:12 +0530

    row

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,53 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+// class MyApp extends StatelessWidget {
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Welcome to Flutter hello world',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Welcome to Flutter hello world'),
+//         ),
+//         body: Center(
+//           child: Text('Hello World by Deepak'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+void main() => runApp(PartTwo());
+
+class PartTwo extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter hello world',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter hello world'),
+    return Row(
+      textDirection: TextDirection.ltr,
+      children: [
+        Expanded(
+          child: Text(
+            'this is test row',
+            textAlign: TextAlign.center,
+            textDirection: TextDirection.ltr,
+          ),
         ),
-        body: Center(
-          child: Text('Hello World by Deepak'),
+        Expanded(
+          child: Text(
+            'this is test row2',
+            textAlign: TextAlign.center,
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.yellow, fontSize: 30.0),
+          ),
         ),
-      ),
+        Expanded(
+          child: Text(
+            'this is test row2',
+            textAlign: TextAlign.center,
+            textDirection: TextDirection.ltr,
+          ),
+        )
+      ],
     );
   }
 }
