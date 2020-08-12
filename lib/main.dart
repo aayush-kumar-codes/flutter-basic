580b3555943e27a8293b033f8659c33561079938
deepak <deepak@excellencetechnologies.in>
2020-08-12 15:41:37 +0530

    images

--- lib/main.dart
+++ lib/main.dart
@@ -79,124 +79,164 @@ import 'package:flutter/material.dart';
 //     );
 //   }
 // }
-void main() => runApp(PartTwo());
+// void main() => runApp(PartTwo());
 
-class PartTwo extends StatelessWidget {
+// class PartTwo extends StatelessWidget {
+//   Widget build(BuildContext context) {
+//     return Column(
+//       textDirection: TextDirection.ltr,
+//       mainAxisSize: MainAxisSize.max,
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: <Widget>[
+//         Flexible(
+//             child: Row(
+//           textDirection: TextDirection.ltr,
+//           children: [
+//             Expanded(
+//                 child: Padding(
+//               padding: const EdgeInsets.all(30.0),
+//               child: Text(
+//                 'ROW1',
+//                 textAlign: TextAlign.center,
+//                 textDirection: TextDirection.ltr,
+//                 style: TextStyle(color: Colors.blue, fontSize: 30.0),
+//               ),
+//             )),
+//             Expanded(
+//                 child: Padding(
+//               padding: const EdgeInsets.all(30.0),
+//               child: Text(
+//                 'ROW2',
+//                 textAlign: TextAlign.center,
+//                 textDirection: TextDirection.ltr,
+//                 style: TextStyle(color: Colors.yellow, fontSize: 30.0),
+//               ),
+//             )),
+//             Expanded(
+//                 child: Padding(
+//               padding: const EdgeInsets.all(30.0),
+//               child: Text(
+//                 'ROW3',
+//                 textAlign: TextAlign.center,
+//                 textDirection: TextDirection.ltr,
+//                 style: TextStyle(color: Colors.red, fontSize: 30.0),
+//               ),
+//             ))
+//           ],
+//         )),
+//         Flexible(
+//             child: Row(
+//           textDirection: TextDirection.ltr,
+//           children: [
+//             Expanded(
+//                 child: Padding(
+//               padding: const EdgeInsets.all(30.0),
+//               child: Text(
+//                 'ROW4',
+//                 textAlign: TextAlign.center,
+//                 textDirection: TextDirection.ltr,
+//                 style: TextStyle(color: Colors.blue, fontSize: 30.0),
+//               ),
+//             )),
+//             Expanded(
+//                 child: Padding(
+//               padding: const EdgeInsets.all(30.0),
+//               child: Text(
+//                 'ROW5',
+//                 textAlign: TextAlign.center,
+//                 textDirection: TextDirection.ltr,
+//                 style: TextStyle(color: Colors.yellow, fontSize: 30.0),
+//               ),
+//             )),
+//             Expanded(
+//                 child: Padding(
+//               padding: const EdgeInsets.all(30.0),
+//               child: Text(
+//                 'ROW6',
+//                 textAlign: TextAlign.center,
+//                 textDirection: TextDirection.ltr,
+//                 style: TextStyle(color: Colors.red, fontSize: 30.0),
+//               ),
+//             ))
+//           ],
+//         )),
+//         Flexible(
+//             child: Row(
+//           textDirection: TextDirection.ltr,
+//           children: [
+//             Expanded(
+//                 child: Padding(
+//               padding: const EdgeInsets.all(30.0),
+//               child: Text(
+//                 'ROW7',
+//                 textAlign: TextAlign.center,
+//                 textDirection: TextDirection.ltr,
+//                 style: TextStyle(color: Colors.blue, fontSize: 30.0),
+//               ),
+//             )),
+//             Expanded(
+//                 child: Padding(
+//               padding: const EdgeInsets.all(30.0),
+//               child: Text(
+//                 'ROW8',
+//                 textAlign: TextAlign.center,
+//                 textDirection: TextDirection.ltr,
+//                 style: TextStyle(color: Colors.yellow, fontSize: 30.0),
+//               ),
+//             )),
+//             Expanded(
+//                 child: Padding(
+//               padding: const EdgeInsets.all(30.0),
+//               child: Text(
+//                 'ROW9',
+//                 textAlign: TextAlign.center,
+//                 textDirection: TextDirection.ltr,
+//                 style: TextStyle(color: Colors.red, fontSize: 30.0),
+//               ),
+//             ))
+//           ],
+//         )),
+//       ],
+//     );
+//   }
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      textDirection: TextDirection.ltr,
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Flexible(
-            child: Row(
-          textDirection: TextDirection.ltr,
+    return MaterialApp(
+      title: 'Deepak Demo App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Deepak App'),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Expanded(
-                child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Text(
-                'ROW1',
-                textAlign: TextAlign.center,
-                textDirection: TextDirection.ltr,
-                style: TextStyle(color: Colors.blue, fontSize: 30.0),
+              child: Image.asset(
+                'assets/images/im1.jpg',
+                fit: BoxFit.cover,
               ),
-            )),
+            ),
             Expanded(
-                child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Text(
-                'ROW2',
-                textAlign: TextAlign.center,
-                textDirection: TextDirection.ltr,
-                style: TextStyle(color: Colors.yellow, fontSize: 30.0),
+              child: Image.asset(
+                'assets/images/im2.jpg',
+                fit: BoxFit.cover,
               ),
-            )),
+            ),
             Expanded(
-                child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Text(
-                'ROW3',
-                textAlign: TextAlign.center,
-                textDirection: TextDirection.ltr,
-                style: TextStyle(color: Colors.red, fontSize: 30.0),
+              child: Image.asset(
+                'assets/images/im3.jpg',
+                fit: BoxFit.cover,
               ),
-            ))
+            )
           ],
         )),
-        Flexible(
-            child: Row(
-          textDirection: TextDirection.ltr,
-          children: [
-            Expanded(
-                child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Text(
-                'ROW4',
-                textAlign: TextAlign.center,
-                textDirection: TextDirection.ltr,
-                style: TextStyle(color: Colors.blue, fontSize: 30.0),
-              ),
-            )),
-            Expanded(
-                child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Text(
-                'ROW5',
-                textAlign: TextAlign.center,
-                textDirection: TextDirection.ltr,
-                style: TextStyle(color: Colors.yellow, fontSize: 30.0),
-              ),
-            )),
-            Expanded(
-                child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Text(
-                'ROW6',
-                textAlign: TextAlign.center,
-                textDirection: TextDirection.ltr,
-                style: TextStyle(color: Colors.red, fontSize: 30.0),
-              ),
-            ))
-          ],
-        )),
-        Flexible(
-            child: Row(
-          textDirection: TextDirection.ltr,
-          children: [
-            Expanded(
-                child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Text(
-                'ROW7',
-                textAlign: TextAlign.center,
-                textDirection: TextDirection.ltr,
-                style: TextStyle(color: Colors.blue, fontSize: 30.0),
-              ),
-            )),
-            Expanded(
-                child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Text(
-                'ROW8',
-                textAlign: TextAlign.center,
-                textDirection: TextDirection.ltr,
-                style: TextStyle(color: Colors.yellow, fontSize: 30.0),
-              ),
-            )),
-            Expanded(
-                child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Text(
-                'ROW9',
-                textAlign: TextAlign.center,
-                textDirection: TextDirection.ltr,
-                style: TextStyle(color: Colors.red, fontSize: 30.0),
-              ),
-            ))
-          ],
-        )),
-      ],
+      ),
     );
   }
 }
